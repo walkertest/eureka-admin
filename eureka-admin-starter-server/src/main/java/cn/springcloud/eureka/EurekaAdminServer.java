e54880afecaf1b30c4a814faa32830c8ed44007f
walker <zhaoyu@huya.com>
2026-02-03 12:36:08 +0800

    change style

--- eureka-admin-starter-server/src/main/java/cn/springcloud/eureka/EurekaAdminServer.java
+++ eureka-admin-starter-server/src/main/java/cn/springcloud/eureka/EurekaAdminServer.java
@@ -10,7 +10,6 @@ import org.springframework.core.env.SimpleCommandLinePropertySource;
 public class EurekaAdminServer {
 
 	public static void main(String[] args) {
-		System.out.println("test");
 		SpringApplication app = new SpringApplication(EurekaAdminServer.class);
 		SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
 		if (!source.containsProperty("spring.profiles.active")
@@ -19,8 +18,7 @@ public class EurekaAdminServer {
 			// SPRING_PROFILES_ACTIVE
 			app.setAdditionalProfiles("dev");// 默认使用开发环境
 		}
-		System.out.println("test1");
+
 		app.run(args);
-		System.out.println("end");
 	}
 }
---
+++ todo.md
@@ -0,0 +1,8 @@
+# todo
+- run起来  --done 
+- 支持多个eureka集群
+- 升级springboot版本&java版本
+- taf服务部署
+
+# 功能优化
+- inactive之后，状态刷新页面以及状态值.
